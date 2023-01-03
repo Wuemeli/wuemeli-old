09176b646c5689f454884a7627ce100b28f8e193
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2023-01-03 17:34:47 +0100

    Update

--- js/snake.js
+++ js/snake.js
@@ -1,8 +1,3 @@
-/*
-Create by Learn Web Developement
-Youtube channel : https://www.youtube.com/channel/UC8n8ftV94ZU_DJLOLtrpORA
-*/
-
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
