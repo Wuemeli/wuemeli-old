9e408929afb2791966b20834b166ad1ac9cf926f
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2023-01-12 14:30:37 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -95,3 +95,12 @@ if (window.innerWidth > 600) {
 function openGame() {
     window.open('snake.html', 'Snake Game Window', 'height=608,width=608');
   }
+
+//Music
+
+var music = document.getElementById("music");
+
+document.getElementById("play-button").addEventListener("click", function() {
+  music.play();
+});
+
