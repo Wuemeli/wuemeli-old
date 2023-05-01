0115255332cc2cd1de4d6bf4b658b6577aee8b52
Wuemeli <wuemeli@gmail.com>
2023-05-01 13:43:39 +0000

    Update

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,5 +1,10 @@
 const easterEgg = new EasterEgg();
+const easterEgg2 = new EasterEgg();
 
 easterEgg.addEgg([32,32,32,13], () => {
     window.open("https://www.google.com/fbx?fbx=snake_arcade", "Snake Arcade", "width=600,height=600");
+});
+
+easterEgg2.addEgg([13], () => {
+    alert("You found the easter egg!");
 });
