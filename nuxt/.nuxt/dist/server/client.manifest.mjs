d352f5873a5de7e94224a6bd86a1fe696b12c666
Wuemeli <wuemeli@gmail.com>
2023-09-30 21:48:44 +0200

    Delete nuxt/.nuxt directory

--- nuxt/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/wuemeli/Documents/GitHub/wuemeli-website/nuxt/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- nuxt/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/wuemeli/Documents/GitHub/wuemeli-website/nuxt/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
